f1990407c30431f5e6ff03871ed8cee6a554c701
Logue <logue@hotmail.co.jp>
2012-07-05 22:09:08 +0900

    翻訳ファイルの更新

--- wiki-common/locale/pot/getcorepot.bat
+++ wiki-common/locale/pot/getcorepot.bat
@@ -2,6 +2,6 @@
 set PATH=%PATH%;"C:\Program Files (x86)\Poedit\bin"
 echo PukiWiki Advance auto pot file generator for lib directory.
 echo Add gettext path before use.
-xgettext -kT_ --from-code utf-8 -o pukiwiki.pot ../../lib/*.php -L PHP --no-wrap --package-name="PukiWiki Advance" --package-name=1.0 --msgid-bugs-address=logue@hotmail.co.jp --copyright-holder="(c)2010-2011 PukiWiki Advance Developers Team"
+xgettext -kT_ --from-code utf-8 -o pukiwiki.pot ../../lib/*.php -L PHP --no-wrap  --package-name="PukiWiki Advance" --package-version=1.0 --msgid-bugs-address=logue@hotmail.co.jp
 echo Finish.
 pause.
--- wiki-common/locale/pot/getpot.bat
+++ wiki-common/locale/pot/getpot.bat
@@ -1,8 +1,19 @@
 @echo off
+
 set PATH=%PATH%;"C:\Program Files (x86)\Poedit\bin"
 IF "%1" == "" GOTO usage
+IF "%1" == "*" GOTO all
+
 @rem xgettext --from-code=UTF-8 -k_ -o %1.pot ../../plugin/%1.inc.php
-xgettext -kT_gettext -kT_ --from-code utf-8 -o %~n1.pot ../../plugin/%~n1.inc.php -L PHP --no-wrap
+xgettext -kT_gettext -kT_ --from-code utf-8 -o %~n1.pot ../../plugin/%~n1.inc.php -L PHP --no-wrap --package-name="PukiWiki Advance" --package-version=1.0 --msgid-bugs-address=logue@hotmail.co.jp
+goto end
+
+:all
+del -f *.pot
+for /F %%A in ('dir /b ..\..\plugin\*.inc.php') do ( 
+	xgettext -kT_gettext -kT_ --from-code utf-8 -o %%~nA ../../plugin/%%A -L PHP --no-wrap --package-name="PukiWiki Advance" --package-version=1.0 --msgid-bugs-address=logue@hotmail.co.jp
+)
+ren *.inc *.pot
 goto end
 
 :usage
