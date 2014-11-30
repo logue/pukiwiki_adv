02d522cc7c68f4481d87da5fe01bd0ec7346fa43
logue <logue@hotmail.co.jp>
2014-11-30 23:46:38 +0900

    Bad-Behaviorを2.2.16に更新

--- vendor/bad-behavior/bad-behavior-wordpress.php
+++ vendor/bad-behavior/bad-behavior-wordpress.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bad Behavior
-Version: 2.2.15
+Version: 2.2.16
 Description: Deny automated spambots access to your PHP-based Web site.
 Plugin URI: http://bad-behavior.ioerror.us/
 Author: Michael Hampton
